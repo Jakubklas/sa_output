fd1718bd768fa9799a1f0496fa14d28ad034d2d7
Klas <jklas@amazon.co.uk>
2025-11-26 17:07:15 +0000

    sa output offers multiplied & created

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 import os
 
 WAVEPLAN_PATH = r"\\ant\dept-eu\Amazon-Flex-Europe\EU-OE\LPnS\LPnS_Model\OptimizedWavePlan.xlsx"
+COL_MAP = {
+            "Date": "OFD Date",
+            "Station": "Station",
+            "Wave Start": "Wave",
+            "Wave Duration": "Duration",
+            "Cycle": "Cycle",
+            "Service Type": "Service Type"
+        }
--- get_output.py
+++ get_output.py
@@ -20,6 +20,42 @@ class SaOutput:
         return df
     
 
+    def get_sa_outputs(self, waveplan:pd.DataFrame, col_map:dict= COL_MAP, nd_only:bool=True):
+        if nd_only:
+            waveplan = waveplan[waveplan["Cycle"].isin(['CYCLE_1', 'CYCLE_2'])]
+
+        # Get common Date/Station/Cycle/Wave identifier
+        waveplan["Key"] = waveplan["Key"] + waveplan["Wave Number"].astype(str)
+
+        # Iterate throught the df & copy the number of rows * Blocks per wave
+        sa = []
+        counter = 0
+        for idx, row in waveplan.iterrows():
+            
+            for _ in range(row["Blocks"]+1):  # Inclusive range
+                sa.append(row)
+            
+            counter +=1
+            if counter % 50 == 0:
+                print(f"Rows processed {counter}")
+
+        sa = pd.DataFrame(sa)
+        sa.rename(columns=col_map, inplace=True)
+        sa = sa[COL_MAP.values()]
+     
+        return sa
+
+
+    def remove_accepted_offers(self):
+        pass
+
+
+
 if __name__ == "__main__":
     sa = SaOutput(next_week=False)
-    print(sa.read_waveplan().head())
+    print("Reading wave plan...\n")
+    waveplan = sa.read_waveplan()
+    print("Wave Plan read...\n")
+    print("Creating SA offers...\n")
+    sa_out = sa.get_sa_outputs(waveplan, COL_MAP, nd_only=True)
+    print(sa_out.head(40))
